b9a4dbfe51bb6c8a2472da4be0e87c75fa646379
sadomw <dom_tmc_108@hotmail.com>
2014-10-22 18:13:20 +0700

    Update picker.rb

--- picker.rb
+++ picker.rb
@@ -1,11 +1,11 @@
 =begin
-  Function file_engineer is use random name in engineers.txt
+  Function file_engineer use random name in engineers.txt
 =end
 def file_engineer
   file = File.open('engineers.txt') # open engineers.txt
-  textFile = file.readlines # read name in engineers.txt
-  sizeFile = textFile.size + 1 # size from name in engineers.txt
-  random = rand(sizeFile) # random number from engineers.txt
+  textFile = file.readlines # read all name from engineers.txt
+  sizeFile = textFile.size + 1 # size from engineers.txt
+  random = rand(sizeFile)
   textRandom = textFile[random] # random name from engineers.txt
 end
 puts file_engineer
